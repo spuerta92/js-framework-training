e4cdf3ac8b93737842818ff69281955d6e3397e5
Sebastian Puerta Hincapie <spuerta92@gmail.com>
2024-09-08 17:42:33 -0400

    vue cli lec 1

---
+++ VUE/iam-project-vue/src/App.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="app-container">
+    <Header />
+    <Navbar />
+    <Home />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from './components/Header.vue'
+import Footer from './components/Footer.vue'
+import Navbar from './components/Navbar.vue'
+import Home from './components/Home.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Header,
+    Footer,
+    Navbar,
+    Home
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ VUE/modal-project2/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="app-container">
+      <h1>{{message}}</h1>
+      <Modal />
+  </div>
+</template>
+
+<script>
+import Modal from './components/Modal.vue'
+export default {
+  name: 'App',
+  data: () => ({
+    message: 'Welcome to Your Vue.js App'
+  }),
+  components: {
+    Modal
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+h1 {
+  color: red;
+}
+</style>
